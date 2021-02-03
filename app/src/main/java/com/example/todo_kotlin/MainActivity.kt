022032dbe62aadf236a3bcc62195a1794f09ff6b
TheBardia <bardiagoharanpour@rocketmail.com>
2021-02-03 11:35:17 -0500

    ToDo List running locally
    Features currently include:
    1. Adding a new ToDo List
    2. Checking a ToDo marked as done
    3. Deleting all the done ToDo items

--- app/src/main/java/com/example/todo_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/todo_kotlin/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.todo_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toDoAdapter: ToDoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toDoAdapter = ToDoAdapter(mutableListOf())
+
+        rvToDoItems.adapter = toDoAdapter
+        rvToDoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddToDo.setOnClickListener {
+            val todoTitle = etToDoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = ToDo(todoTitle)
+                toDoAdapter.addToDo(todo)
+                etToDoTitle.text.clear()
+            }
+
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+
+        btnDeleteDoneToDos.setOnClickListener {
+            toDoAdapter.deleteDoneToDos()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todo_kotlin/ToDo.kt
@@ -0,0 +1,6 @@
+package com.example.todo_kotlin
+
+data class ToDo (
+    val title: String,
+    var isChecked: Boolean = false
+)
